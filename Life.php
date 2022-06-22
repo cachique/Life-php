8d75286e4a27b4b467badb6b56b188c126d46968
Walter Cattebeke <cachique@gmail.com>
2022-06-21 22:31:10 -0400

    Procedimiento para crear el grid vacio

--- Life.php
+++ Life.php
@@ -2,6 +2,17 @@
 
     class Life {
         public $grid = [];
+
+        function createGrid(){
+            for ($i=1; $i <= 25 ; $i++) { 
+                # code...
+                $height = [];
+                for ($j=1; $j <= 50 ; $j++) { 
+                    # code...
+                    $height[$j] = 0;
+                }
+                $this->grid[$i] = $height;
+            }
+        }
     }
-    
 ?>
